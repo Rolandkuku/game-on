dacf87eb0fed72ab7220d841a7930db7d6146f4f
Roland Kullet <renaud.bellec.3@gmail.com>
2016-10-26 17:28:43 +0200

    somewhere over the rainbow

--- public/js/lib/app.js
+++ public/js/lib/app.js
@@ -1,1 +1,48 @@
-console.log("app.js");
+import React from "react";
+import { createStore, Component } from "redux";
+
+const userActions = {
+    SET_USER_NAME: "SET_USER_NAME",
+    SET_USER_PASSWORD: "SET_USER_PASSWORD"
+};
+
+class User {
+    constructor() {
+        this.username = "";
+        this.password = "";
+    }
+
+    set username(username) {
+        if (username) {
+            console.debug("username as just been set to " + username);
+            this.username = username;
+        } else {
+            console.warn("No username specified");
+        }
+    }
+
+    set password(password) {
+        if (username) {
+            console.debug("password as just been set to " + password);
+            this.password = password;
+        } else {
+            console.warn("No password specified");
+        }
+    }
+}
+
+const user = new User();
+
+const setUserName = (username) => {
+    return {
+        type: SET_USER_NAME,
+        username: username
+    }
+}
+
+const setUserPassword = (password) => {
+    return {
+        type: SET_USER_PASSWORD,
+        password: password
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    //'http://localhost:3000',
+    './public/js/lib/app.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'public/dist'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'public/js/lib')
+    }]
+  }
+};
